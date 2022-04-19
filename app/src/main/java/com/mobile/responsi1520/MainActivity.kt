55bdf02adbb3d20f80dfd99074384ff5d8214873
pepenpriadi <pepenpriadi060@gmail.com>
2022-04-19 23:22:35 +0700

    Initial commit

---
+++ app/src/main/java/com/mobile/responsi1520/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.mobile.responsi1520
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+     
+    }
+}
---
+++ app/src/main/java/com/mobile/responsi1520/RecoveryActivity.kt
@@ -0,0 +1,11 @@
+package com.mobile.responsi1520
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecoveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery)
+    }
+}
